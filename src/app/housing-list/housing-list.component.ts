41a2d0a506146b053a0f9968dc3088f233da084d
tintayadev <pau.tintaya.code@gmail.com>
2024-11-13 08:43:18 -0400

    add changes

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -21,7 +21,11 @@ export class HousingListComponent implements OnInit {
   searchHousingLocations(searchText: string) {
     if (!searchText) return;
 
-    this.results = this.locationList.filter((location: any) => location.city.toLowerCase().includes(searchText.toLowerCase()));
+    this.results = this.locationList.filter((location: HousingLocation) =>
+      location.city.toLowerCase().includes(searchText.toLowerCase()) ||
+      location.name.toLowerCase().includes(searchText.toLowerCase()) ||
+      location.state.toLowerCase().includes(searchText.toLowerCase())
+    );
   }
 
   selectHousingLocation(location: HousingLocation) {
